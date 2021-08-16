47686a8e85d5533a44e2c155e11b909b6d15cd95
padma-pani-patra <padmapanipatra@gmail.com>
2021-08-16 21:23:19 +0530

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,26 @@
 import 'package:flutter/material.dart';
 
 import 'pages/register.dart';
-import 'sidebar/sidebar_layout.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    theme: ThemeData(
-        scaffoldBackgroundColor: Color(0xff0D1F35),
-        primaryColor: Color(0xff0D1F35)),
-    home: Register(), // Register Page
-    // home: SidebarLayout(),
-  ));
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).requestFocus(new FocusNode());
+      },
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+            scaffoldBackgroundColor: Color(0xff0D1F35),
+            primaryColor: Color(0xff0D1F35)),
+        home: Register(), // Register Page
+        // home: SidebarLayout(),
+      ),
+    );
+  }
 }
--- lib/pages/signup.dart
+++ lib/pages/signup.dart
@@ -285,7 +285,7 @@ class _SignupState extends State<Signup> {
             ),
           ),
           SizedBox(
-            height: 73,
+            height: height * .12,
           ),
           Align(
             alignment: Alignment.bottomCenter,
